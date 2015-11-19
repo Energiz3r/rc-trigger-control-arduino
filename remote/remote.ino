a98238a9bdb1f9b87cdb63a08e3269eaec9623e7
Yale Maloney <yale81@gmail.com>
2015-11-19 19:57:21 +1100

    Moved config to it's own file

--- remote/remote.ino
+++ remote/remote.ino
@@ -6,65 +6,7 @@
 //TO DO:
 // -Add support for per-channel upper/lower/centre limits (useful if ESCs or motors are not identical)
 
-// ---CONFIG START---
-
-//serial output. 0 = off, 1 = status/output display, 2 = show PWM values (for calibrating receiver)
-const int serial_output = 2;
-
-//swaps left/right output channels
-const boolean channel_swap = false;
-
-//reverse steering direction while moving forward
-const boolean steering_swap = false;
-//reverse steering direction while moving in reverse
-const boolean steering_reverse_swap = false;
-
-//the maximum difference (in PWM value) each cycle to allow the input to change by. This prevents spikes in PWM input from instantly stopping or changing wheel direction (smoothes stop/go)
-const int difference = 50;
-//enable or disable this setting
-const boolean rate_change_limit = true;
-
-//this value alters the neutral position and upper/lower limits of the ESC output range
-const float outputcentre = 90; //limits are usually 0-180 degrees where 90 is neutral for an ESC. adjust if bot moves while throttle is neutral
-const float outputmax = 130; //the upper/lower values shouldn't need to be changed unless your ESC is strange
-const float outputmin = 40; //I am using 130 & 40 as the ESC cuts out when the full range is used. defaults = 90, 180, 0
-
-//alter this to change how the wheels on the turning side will behave
-//a setting of 2 will set the turning side to full reverse when steering is at full lock (allows the bot to turn on the spot)
-//a setting of 1 won't reverse the wheels on the turning side, only stop them (bot will turn in an arc)
-//supports float values between 0 and 2
-const float steering_sensitivity = 2; //default value is 2
-
-//these are the values as measured for an HPI RF-40 R/C receiver. use serial_output = 2 to determine the correct values for your receiver
-const float throt_neutral_upper = 1500; //these values represent the PWM width the receiver outputs while in neutral positions.
-const float throt_neutral_lower = 1400; //it fluctuates so we specify a range (upper and lower limits)
-const float steer_neutral_upper = 1550; //if you can't get your bot to stay still with throttle/steering in neutral, widen these ranges
-const float steer_neutral_lower = 1400;
-const float throt_upper = 1900; //these next values represent the entire range of input
-const float throt_lower = 1000;
-const float steer_upper = 1800;
-const float steer_lower = 1100; //don't forget to set the values in the next section for when the TX is disconnected
-
-//this value alters the sensitivity of the safety cut off (when receiver loses connection to transmitter). you shouldn't need to change this
-//this works by detecting when steering and throttle are both within a certain range - the values the receiver outputs while the transmitter is turned off
-//your receiver may behave differently - if that's the case you can ignore this setting.
-const int lostconwaittime = 500; //time (in ms) to wait before setting outputs to neutral positions if receiver is outputting these ranges continuously
-
-//these are the output values from the receiver for when the transmitter is off or out of range. make sure you use a range that will capture the state consistently (it can fluctuate)
-const int throt_lostcon_upper = 1700; //throttle
-const int throt_lostcon_lower = 1600;
-const int steer_lostcon_upper = 1200; //steering
-const int steer_lostcon_lower = 1000;
-
-//configure digital pins on arduino
-const int esc_left_output = 22; //connect to left ESC signal wire
-const int esc_right_output = 24; //right ESC signal wire
-const int rx_throt_input = 28; //signal wire for receiver throttle (usually channel 2)
-const int rx_steer_input = 30; //receiver steering (usually channel 1)
-
-//most ESCs will provide ~6V to the receiver which you can also use to power the arduino. Connect red wire from ESC or receiver to +5V header or barrel connector on arduino
-
-// ---END CONFIG---
+#include "config.h"
 
 int throtpos = (throt_neutral_upper - throt_neutral_lower) / 2; //stores the current positions for the steering and throttle PWM values
 int steerpos = (steer_neutral_upper - steer_neutral_lower) / 2;
